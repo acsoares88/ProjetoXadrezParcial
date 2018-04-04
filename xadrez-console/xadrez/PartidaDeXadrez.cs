3865dce404c575933ee8a963b931db65dd7d3de0
Anderson Soares <anderson.conceicao.soares@gmail.com>
2018-04-03 21:31:57 -0300

    Implementando as restricoes de movimento

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -6,8 +6,8 @@ namespace xadrez
      class PartidaDeXadrez
     {
         public Tabuleiro tab { get; set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual { get; private set;}
         public bool terminada { get; private set; }
 
         public PartidaDeXadrez()
@@ -42,6 +42,42 @@ namespace xadrez
             tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('e', 7).toPosicao());
             tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('e', 8).toPosicao());
             tab.colocarPeca(new Rei(tab, Cor.Preta), new PosicaoXadrez('d', 8).toPosicao());
+        }
+
+        public void realizaJogada(Posicao origem, Posicao destino){
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+
+        public void mudaJogador(){
+            if (jogadorAtual == Cor.Branca) {
+                jogadorAtual = Cor.Preta;
+            }else {
+                jogadorAtual = Cor.Branca;
+            }
+        }
+
+        public  void validarPosicaoDeOrigem(Posicao pos){
+
+            if (tab.peca(pos) == null){
+                throw new TabuleiroException("Não existe peça na posicao escolhida");
+            }
+
+            if (jogadorAtual != tab.peca(pos).cor){
+                throw new TabuleiroException("A peca de origem escolhida não é a sua");
+            }
+
+            if (tab.peca(pos).exiteMovimentosPossiveis() == false)
+            {
+                throw new TabuleiroException("Não ecxiste jogada disponivel para peça selecionada");
+            }
+        }
+
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino){
+            if (tab.peca(origem).podeMoverPara(destino) == false){
+                throw new TabuleiroException("Posicao de Destino inválida");
+            }
         }
     }
 }
